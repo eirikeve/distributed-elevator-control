3ee5a91089bebdb5d50c568897b0c752e0702fb6
Ole Petter <olepno@stud.no>
2018-02-21 12:06:21 +0100

    Started implementing heartbeat

--- src/elevnetwork/elevheartbeat.go
+++ src/elevnetwork/elevheartbeat.go
@@ -1,1 +1,24 @@
 package elevnetwork
+
+import(
+	"./conn"
+	"./bcast"
+	"./localip"
+	"./peers"
+	"time"
+	"encoding/json"
+	"net"
+)
+
+const HEARTBEATINTERVAL = 100*time.Millisecond
+const HEARTBEATTIMEOUT = 10 * HEARTBEATINTERVAL
+
+type struct HeartbeatMessage{
+	Status string json:"status"
+	// IP/Address ? 	
+}
+
+func udpSendHeartBeat(port string,peerListLocalCh chan []string){
+	localIP, err := LocalIP()
+	//get staus 
+}
