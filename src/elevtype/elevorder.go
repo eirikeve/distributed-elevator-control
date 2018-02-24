cfd499ed4145de206803d4800adfc08f2b634f28
student@ntnu.no <student@ntnu.no>
2018-02-24 14:51:59 +0100

    Definitions for Elevator and ElevOrder

--- src/elevtype/elevorder.go
+++ src/elevtype/elevorder.go
@@ -1,32 +1,27 @@
 package elevtype
 
+// OrderStatus is the current state of an elevator order
 type OrderStatus int
 
 const (
-	Unknown    OrderStatus = 0
-	Unassigned OrderStatus = 1
-	Assigned   OrderStatus = 2
-	Timeout    OrderStatus = 3
-	Finished   OrderStatus = 4
+	Timeout  OrderStatus = -2
+	Unknown  OrderStatus = -1
+	Received OrderStatus = 0
+	Accepted OrderStatus = 1
+	Finished OrderStatus = 2
 )
 
-type OrderInfo struct {
-	S         OrderStatus
-	timestamp int64
-	assignee  string
-}
-
-type Elevorder struct {
+type ElevOrder struct {
 	// ID of request IP+timestamp+ButtonEvent
 	Id string
 	// Floor & order type
 	Order ButtonEvent
 	// Timestamp at order creation [UNIX timestamp]
-	TimestampCreations int64
+	TimestampReceived int64
 	// Current status of order
 	Status OrderStatus
 	// Last update of order [UNIX timestamp]
-	TimestampOrderChange int64
+	TimestampLastOrderStatusChange int64
 	// Assigned to elev @ IP
 	Assignee string
 }
