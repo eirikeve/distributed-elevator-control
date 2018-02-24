4959d26dbdcf6ba03c18630d45a3c4070eb07855
student@ntnu.no <student@ntnu.no>
2018-02-24 17:12:02 +0100

    Function interfaces for FSM

---
+++ src/elevfsm/elevfsm.go
@@ -0,0 +1,65 @@
+package elevfsm
+
+import (
+	"../elevtype"
+)
+
+var elevator elevtype.Elevator
+var lastRegisteredFloor int
+
+// Functions for running the local elevator
+
+func GetMovementDirection() elevtype.MotorDirection {
+	var v elevtype.MotorDirection
+
+	return v
+
+}
+
+func GetPanelLights() []elevtype.ButtonLamp {
+	var v []elevtype.ButtonLamp
+
+	return v
+}
+
+func RegisterFloor() {
+
+}
+
+func GetState() elevtype.ElevatorState {
+	v := elevtype.Idle
+
+	return v
+
+}
+
+func setState(state elevtype.ElevatorState) {
+
+}
+
+func setDir(dir elevtype.MotorDirection) {
+
+}
+
+func unload() {
+
+}
+
+func idle() {
+
+}
+
+// Functions used when interfacing with elevNetworkHandler
+func PushRequestToQueue(order elevtype.ElevOrder) {
+
+}
+func RemRequestFromQueue(order elevtype.ElevOrder) {
+
+}
+
+func GetElevator() elevtype.Elevator {
+	var v elevtype.Elevator
+
+	return v
+
+}
