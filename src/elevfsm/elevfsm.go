b63a76b8e36a50139c157c0773cf3390a32a6f35
Eirik Vesterkjær <eve.eirik@gmail.com>
2018-03-06 16:53:15 +0100

    Initialize

--- src/elevfsm/elevfsm.go
+++ src/elevfsm/elevfsm.go
@@ -1,9 +1,29 @@
 package elevfsm
 
-import et "../elevtype"
+import (
+	et "../elevtype"
+	log "github.com/sirupsen/logrus"
+)
 
 var elevator et.Elevator
 var lastRegisteredFloor int
+var timerSignal chan<- bool
+
+func Initialize(timerSignalOutput chan<- bool, e * Elev) {
+	timerSignal = timerSignalOutput;
+	if (e == nil) {
+		elevator = et.Elevator{
+			Floor:              4,
+			MovementDirection:   et.MD_Stop,
+			MovDirFromLastFloor: et.MD_Up,
+			State:               et.Initializing,
+			ErrorState:          et.FullFunctionality}
+		log.WithField("elevator", elevator).Debug("elevfsm Initialize: No ref, reinitialized elevator")
+	} else {
+		elevator = &e
+		log.WithField("elevator", elevator).Debug("elevfsm Initialize: Initialized elevator from ref")
+	}
+}
 
 // Functions for running the local elevator
 /*
@@ -19,8 +39,10 @@ func GetPanelLights() []et.ButtonLamp {
 
 func RegisterFloor(floor int) {
 	if !(isValidFloor(floor)) {
+		log.WithField("floor", floor).Error("elevfsm RegisterFloor: Invalid input")
 		return
 	}
+	log.WithField("floor", floor).Debug("elevfsm RegisterFloor:")
 	updateFloor(floor)
 	if OrderLogicCheckShouldStopAtFloor(elevator) {
 		// need a good way to make sure we start moving after some time
@@ -53,7 +75,6 @@ func idle() {
 }
 
 func updateFloor(floor int) {
-	// log here
 	lastRegisteredFloor = elevator.Floor
 	elevator.Floor = floor
 }
@@ -76,3 +97,5 @@ func GetElevator() et.Elevator {
 	return v
 
 }
+
+isTimeout()
