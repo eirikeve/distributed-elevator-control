624f275b6ca88f93b28a21624f79784d07a88e03
Eirik Vesterkjær <eve.eirik@gmail.com>
2018-03-06 18:07:51 +0100

    Added Stop and Update for timers, along with changes for thread safety

--- src/elevtimer/elevtimer.go
+++ src/elevtimer/elevtimer.go
@@ -1,10 +1,17 @@
 package elevtimer
 
 import (
+	"sync"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 )
+
+// Map for updating the duration of timers
+var timers = make(map[string]chan time.Duration)
+
+// Mutex for thread safety. Used due to accessing timers, which can cause unexpected behaviour
+var lock sync.Mutex
 
 /*Start creates a timer instance which signals true on signalTimeout channel after duration time has passed.
  * timerName is just an identifier for when logging info about the timer.
@@ -14,32 +21,81 @@ import (
  * @arg timerName: timer identifier, can be passed as "" if desired. Only used for logging purposes.
  */
 func Start(signalTimeout chan bool, duration time.Duration, timerName string) {
-	go timerInstance(signalTimeout, duration, timerName)
-	log.WithField("timerName", timerName).Debug("elevtimer Start: Timer start")
+	lock.Lock()
+	defer lock.Unlock()
+	_, exists := timers[timerName]
+	if exists {
+		log.WithFields(log.Fields{"timerName": timerName, "duration": duration}).Warning("elevtimer Start: Called start on existing timer, updating its duration instead")
+
+	} else {
+		timers[timerName] = make(chan time.Duration, 2)
+		go timerInstance(signalTimeout, duration, timerName, timers[timerName])
+		log.WithFields(log.Fields{"timerName": timerName, "duration": duration}).Debug("elevtimer Start: Timer start")
+	}
+}
+
+/*Update sets a new timeout for timer timerName
+ *newDuration is the time from time.Now() until the timeout of the timer.
+ * @arg timerName: timer identifier
+ * @arg newDuration: new time until timeout
+ */
+func Update(timerName string, newDuration time.Duration) {
+	lock.Lock()
+	defer lock.Unlock()
+	c, exists := timers[timerName]
+	if exists {
+		// @TODO possible error here if c is non-existent due to some bug
+
+		log.WithFields(log.Fields{"timerName": timerName, "duration": newDuration}).Debug("elevtimer Update: New duration")
+		c <- newDuration
+
+	} else {
+		log.WithField("timerName", timerName).Error("elevtimer Update: Not found")
+	}
+}
+
+/*Stop is a wrapper for Update(timerName,0)
+ * @arg timerName: timer identifier
+ */
+func Stop(timerName string) {
+	Update(timerName, time.Second*0)
 }
 
 /*timerInstance is an instance which signals true to signalTimeout after duration time.
  * It is created in the Start function, you don't need to call this function directly.
  * @arg signalTimeout: channel where the timer signals when it timeouts.
  * @arg duration: the duration from the function call until the timerInstance signals timeout
- * @arg timerName: timer identifier, can be passed as "" if desired. Only used for logging purposes.
+ * @arg timerName: timer identifier
+ * @arg durationUpdateChan: used to send new durations to the timerInstance, when calling Update or Stop
  */
-func timerInstance(signalTimeout chan<- bool, duration time.Duration, timerName string) {
-	var trySendTimeoutCount = 0
+func timerInstance(signalTimeout chan<- bool, duration time.Duration, timerName string, durationUpdateChan <-chan time.Duration) {
 	const trySendTimeoutLimit = 100
+	var trySendTimeoutCount = 0
+	var startTime = time.Now()
 
-	if duration > time.Second*0 {
-		time.Sleep(duration)
+	// Counting until duration has passed. Listen for updates to duration.
+	for time.Now().Sub(startTime) < duration {
+		select {
+		case newDuration := <-durationUpdateChan:
+			startTime = time.Now()
+			duration = newDuration
+			log.WithField("timerName", timerName).Debug("elevtimer timerInstance: Timer duration updated")
+		default:
+			time.Sleep(time.Millisecond * 50)
+		}
 	}
 
+	// Timeout! Send message on signalTimeout channel
 	for {
 		select {
 		case signalTimeout <- true:
 			log.WithField("timerName", timerName).Debug("elevtimer timerInstance: Timer timeout signalled. Shutting down timer instance")
+			removeTimer(timerName)
 			return
 		default:
 			if trySendTimeoutCount > trySendTimeoutLimit {
 				log.WithField("timerName", timerName).Error("elevtimer timerInstance: Unable to signal. Shutting down timer instance")
+				removeTimer(timerName)
 				return
 			} else {
 				trySendTimeoutCount++
@@ -52,5 +108,19 @@ func timerInstance(signalTimeout chan<- bool, duration time.Duration, timerName 
 			}
 
 		}
+	}
+}
+
+/*removeTimer deletes a timerName and its corresponding durationUpdateChan from the timers map
+ * @arg timerName: identity of timer to be removed
+ */
+func removeTimer(timerName string) {
+	lock.Lock()
+	defer lock.Unlock()
+	_, exists := timers[timerName]
+	if exists {
+		delete(timers, timerName)
+	} else {
+		log.WithField("timerName", timerName).Error("elevtimer removeTimer: Cannot find timer")
 	}
 }
