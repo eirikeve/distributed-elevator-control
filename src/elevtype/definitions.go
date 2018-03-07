29a0a2571ebabfe850e91e2e0f33856fbf665a28
Eirik Vesterkjær <eve.eirik@gmail.com>
2018-03-07 17:17:08 +0100

    Added definitions file

---
+++ src/elevtype/definitions.go
@@ -0,0 +1,10 @@
+package elevtype
+
+//Debugging: if true, log trace statements
+const VerboseDebugLog = false
+
+// Elevator
+const NumFloors = 4
+const NumButtons = 3
+const BOTTOMFLOOR = 0
+const TOPFLOOR = 3
