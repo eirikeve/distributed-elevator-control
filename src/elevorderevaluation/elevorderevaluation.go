7c8b6ddb18cdfb1526f52c74a18424ced47ce80f
Eirik Vesterkjær <eve.eirik@gmail.com>
2018-03-09 17:23:45 +0100

    Fix bugs after merge

--- src/elevorderevaluation/elevorderevaluation.go
+++ src/elevorderevaluation/elevorderevaluation.go
@@ -1,10 +1,11 @@
 package elevorderevaluation
 
-import(
-    et "../elevtype"
-    fsm "../elevfsm"
-    "fmt"
-    )
+import (
+	"fmt"
+
+	fsm "../elevfsm"
+	et "../elevtype"
+)
 
 const TRAVEL_TIME = 3
 const DOOR_OPEN_TIME = 5
@@ -13,86 +14,86 @@ const DOOR_OPEN_TIME = 5
 * Calculates how much time the Elevator will use to execute
 * all its order, thus going into the Idle state
 * @arg elev: Takes an Elevator as arguemtent, making it possible to simalute its actions
-*/ 
-func timeToIdle(elev et.Elevator) int{
-    duration := 0
+ */
+func timeToIdle(elev et.Elevator) int {
+	duration := 0
 	isSimulating := true
 
-    switch(elev.State){
-        case et.Idle:
-            elev.MovementDirection = fsm.OrderLogicGetMovementDirection(elev);
-            if elev.MovementDirection == et.MD_Stop{
-                return duration
-            }
-        case et.Moving:
-            duration += TRAVEL_TIME/2;
-            elev.Floor += int(elev.MovementDirection);          //[BUG] Not sure if converts motor type to int to floor
-        case et.Unloading:                         
-            duration+=DOOR_OPEN_TIME/2
-        default:
-            //Should not be possible to enter default
-            println("Entered defualt in timeToIdle. This should not happen")
-    }
-	for isSimulating == true{
-        if fsm.OrderLogicCheckShouldStopAtFloor(elev) == true{
-            elev = fsm.OrderLogicClearRequestsOnCurrentFloor(elev,elev.MovementDirection)
-            duration += DOOR_OPEN_TIME;
-            elev.MovementDirection = fsm.OrderLogicGetMovementDirection(elev);
-            if(elev.MovementDirection == et.MD_Stop){
-                return duration;
-            }
-        }
-        elev.Floor += int(elev.MovementDirection);
-        duration += TRAVEL_TIME;                    //[POTENTIAL BUG] Not sure if converts motor type to int to floor
+	switch elev.State {
+	case et.Idle:
+		elev.MovementDirection = fsm.OrderLogicGetMovementDirection(elev)
+		if elev.MovementDirection == et.MD_Stop {
+			return duration
+		}
+	case et.Moving:
+		duration += TRAVEL_TIME / 2
+		elev.Floor += int(elev.MovementDirection) //[BUG] Not sure if converts motor type to int to floor
+	case et.Unloading:
+		duration += DOOR_OPEN_TIME / 2
+	default:
+		//Should not be possible to enter default
+		println("Entered defualt in timeToIdle. This should not happen")
 	}
-    
-    return duration
-} 
+	for isSimulating == true {
+		if fsm.OrderLogicCheckShouldStopAtFloor(elev) == true {
+			elev = fsm.OrderLogicClearRequestsOnCurrentFloor(elev, elev.MovementDirection)
+			duration += DOOR_OPEN_TIME
+			elev.MovementDirection = fsm.OrderLogicGetMovementDirection(elev)
+			if elev.MovementDirection == et.MD_Stop {
+				return duration
+			}
+		}
+		elev.Floor += int(elev.MovementDirection)
+		duration += TRAVEL_TIME //[POTENTIAL BUG] Not sure if converts motor type to int to floor
+	}
+
+	return duration
+}
 
 /*
 * Takes a list of Elevators, and simulates them all finding which elevator
 * is best fit to take and execute an order.
 * @arg elev[]: List of Elevators
-*/
-func delegateOrder(elevList []et.Elevator, newOrder et.ElevOrder) int {
-    var durations[] int
+ */
+func delegateOrder(elevList []et.Elevator, newOrder et.SimpleOrder) int {
+	var durations []int
 
+	for _, elev := range elevList {
+		elev = insertElevatorOrder(elev, newOrder)
+		tempDuration := timeToIdle(elev)
+		durations = append(durations, tempDuration)
+	}
+	for index, element := range durations {
+		fmt.Printf("delegateOrder, Elevator  %v: Duration: %v \n", index, element)
+	}
 
-    for _, elev :=range elevList{
-        elev = insertElevatorOrder(elev,newOrder)
-        tempDuration:=timeToIdle(elev)
-        durations = append(durations,tempDuration)
-    }
-    for index,element :=range durations{
-        fmt.Printf("delegateOrder, Elevator  %v: Duration: %v \n",index,element)
-    }
-
-    optElevIndex := findMinIndex(durations)
-    return optElevIndex
-} 
+	optElevIndex := findMinIndex(durations)
+	return optElevIndex
+}
 
 /*
 * Takes a list of integers and returns the index
 * containing the largest value
-* @arg list: list containing integers 
-*/
-func findMinIndex(list []int) int{
-    
-    var maxIndex int;
-    var maxValue int;
-    if list == nil{
-        println("Empty list, Crashing")
-    }
-    for index,element :=range list{
-        if (index == 0){
-            maxValue = element
-            maxIndex = index}
-        if (element < maxValue){
-            maxValue = element
-            maxIndex = index
-        }
-    }
-    return maxIndex
+* @arg list: list containing integers
+ */
+func findMinIndex(list []int) int {
+
+	var maxIndex int
+	var maxValue int
+	if list == nil {
+		println("Empty list, Crashing")
+	}
+	for index, element := range list {
+		if index == 0 {
+			maxValue = element
+			maxIndex = index
+		}
+		if element < maxValue {
+			maxValue = element
+			maxIndex = index
+		}
+	}
+	return maxIndex
 }
 
 /*
@@ -104,7 +105,7 @@ func printElevatorQueue(elev et.Elevator) {
 	for floor := 0; floor < et.NumFloors; floor++ {
 		fmt.Printf("Floor %v: \t  ", floor)
 		for button := 0; button < et.NumButtons; button++ {
-			if elev.Orders[floor][button].Status == et.Accepted {
+			if elev.Orders[floor][button].IsActive() {
 				print("TRUE \t\t  ")
 			} else {
 				print("FALSE \t  ")
@@ -115,14 +116,13 @@ func printElevatorQueue(elev et.Elevator) {
 
 }
 
-
 /*
 * Sets a order in the Elevator queue at the given location
 * @arg elev: Elevator
 * @arg ElevOrder: The new Elevator Order
 * @arg ButtonEvent: Which button was triggered and at which floor is was pressed
-*/
-func insertElevatorOrder(elev et.Elevator, order et.ElevOrder) et.Elevator {
+ */
+func insertElevatorOrder(elev et.Elevator, order et.SimpleOrder) et.Elevator {
 	elev.Orders[order.Order.Floor][order.Order.Button] = order
 	return elev
 }
--- src/elevorderevaluation/elevorderevaluation_test.go
+++ src/elevorderevaluation/elevorderevaluation_test.go
@@ -1,84 +1,77 @@
 package elevorderevaluation
 
-import(
-	"testing"
-	et "../elevtype"
+import (
 	"fmt"
+	"testing"
+
+	et "../elevtype"
 )
-
-
-
 
 /*
 * Test if finding maximum index, i.e. element with largest integer value, is working properly
-*/
-func TestMinIndex(t *testing.T){
-	durations := []int{27,8,5}
+ */
+func TestMinIndex(t *testing.T) {
+	durations := []int{27, 8, 5}
 	index := findMinIndex(durations)
-	println("MinIndex:",index)
+	println("MinIndex:", index)
 	println("Containg the value", durations[index])
 }
 
-func TestTimeToIdle(t *testing.T){
+func TestTimeToIdle(t *testing.T) {
 	// Initialize elevator at Floor 1
-	elevOne:= initializeElevator(1)
+	elevOne := initializeElevator(1)
 	elevOne.MovementDirection = et.MD_Up
 	elevOne.State = et.Unloading
 	// Set Queue for elevator
-	elevOne = setElevatorOrder(elevOne, 1, et.BT_HallUp, "1", et.Accepted)
-	elevOne = setElevatorOrder(elevOne,0,et.BT_Cab,"2",et.Accepted)
-	elevOne = setElevatorOrder(elevOne,3,et.BT_HallDown,"3", et.Accepted)
+	elevOne = setElevatorOrder(elevOne, 1, et.BT_HallUp, "1")
+	elevOne = setElevatorOrder(elevOne, 0, et.BT_Cab, "2")
+	elevOne = setElevatorOrder(elevOne, 3, et.BT_HallDown, "3")
 
 	printElevatorQueue(elevOne)
-	
+
 	// Get duration
 	fmt.Printf("\n")
-	duration:= timeToIdle(elevOne)
-	fmt.Printf("TimeToIdle, Duration: %v \n\n\n",duration)
+	duration := timeToIdle(elevOne)
+	fmt.Printf("TimeToIdle, Duration: %v \n\n\n", duration)
 }
 
-func TestOrderDelegation(t *testing.T){
+func TestOrderDelegation(t *testing.T) {
 	// Initialize elevator at Floor 1
-	elevOne:= initializeElevator(2)
-	elevTwo:= initializeElevator(0)
-	elevThree:= initializeElevator(1)
-	
+	elevOne := initializeElevator(2)
+	elevTwo := initializeElevator(0)
+	elevThree := initializeElevator(1)
+
 	// Set-up for elevator 1
 	elevOne.MovementDirection = et.MD_Up
 	elevOne.State = et.Moving
-	elevOne = setElevatorOrder(elevOne, 1, et.BT_HallUp, "1", et.Accepted)
-	elevOne = setElevatorOrder(elevOne,0,et.BT_Cab,"2",et.Accepted)
-	elevOne = setElevatorOrder(elevOne,2,et.BT_HallDown,"3", et.Accepted)
-	
+	elevOne = setElevatorOrder(elevOne, 1, et.BT_HallUp, "1")
+	elevOne = setElevatorOrder(elevOne, 0, et.BT_Cab, "2")
+	elevOne = setElevatorOrder(elevOne, 2, et.BT_HallDown, "3")
+
 	// Set-up for elevator 2
-	elevTwo = setElevatorOrder(elevTwo, 1, et.BT_HallUp, "1", et.Accepted)
-	elevTwo = setElevatorOrder(elevTwo,3,et.BT_Cab,"2",et.Accepted)
-	elevTwo = setElevatorOrder(elevTwo,1,et.BT_HallDown,"3", et.Accepted)
+	elevTwo = setElevatorOrder(elevTwo, 1, et.BT_HallUp, "1")
+	elevTwo = setElevatorOrder(elevTwo, 3, et.BT_Cab, "2")
+	elevTwo = setElevatorOrder(elevTwo, 1, et.BT_HallDown, "3")
 
 	// Set-up for elevator 3
 	elevThree.MovementDirection = et.MD_Up
 	elevThree.State = et.Unloading
-	elevThree = setElevatorOrder(elevThree, 1, et.BT_HallUp, "1", et.Accepted)
-	elevThree = setElevatorOrder(elevThree,0,et.BT_Cab,"3", et.Accepted)
+	elevThree = setElevatorOrder(elevThree, 1, et.BT_HallUp, "1")
+	elevThree = setElevatorOrder(elevThree, 0, et.BT_Cab, "3")
 
 	//New Order
 	bEvent := et.ButtonEvent{3, et.BT_HallDown}
-	newOrder := et.ElevOrder{"NewOrder", bEvent, 2, et.Accepted, 2, "Elev"}
+	newOrder := et.SimpleOrder{"NewOrder", bEvent}
 
 	listElev := []et.Elevator{elevOne, elevTwo, elevThree}
-	bestElev:= delegateOrder(listElev,newOrder)
+	bestElev := delegateOrder(listElev, newOrder)
 
 	fmt.Printf("TestOrderDelegation, Best elevator: %v \n", bestElev)
 
-
 }
 
-
-
-
-
 //[@todo]: Most of these functions are used in "elevfsm/orderlogic_test", maybe they should be imported?
- 
+
 func initializeElevator(startingFloor int) et.Elevator {
 	// Initialize a Elevator
 	var elev et.Elevator
@@ -105,14 +98,13 @@ func initializeElevator(startingFloor int) et.Elevator {
 func initilizeElevatorQueue(elev et.Elevator) et.Elevator {
 	for floor := 0; floor < et.NumFloors; floor++ {
 		for button := 0; button < et.NumButtons; button++ {
-			elev.Orders[floor][button] = et.EmptyOrder()
+			elev.Orders[floor][button] = et.SimpleOrder{}
 		}
 	}
 
 	return elev
 
 }
-
 
 /*
 * Sets a order in the Elevator queue at the given location
@@ -121,10 +113,9 @@ func initilizeElevatorQueue(elev et.Elevator) et.Elevator {
 * @arg button: Set which Buttontype that was pressed
 * @arg ID: Give unique ID for the Order
 * @arg stat: Set status for Order
-*/
-func setElevatorOrder(elev et.Elevator, floor int, button et.ButtonType, ID string, stat et.OrderStatus) et.Elevator {
+ */
+func setElevatorOrder(elev et.Elevator, floor int, button et.ButtonType, ID string) et.Elevator {
 	bEvent := et.ButtonEvent{floor, button}
-	elev.Orders[floor][button] = et.ElevOrder{ID, bEvent, 2, stat, 2, "Elev"}
+	elev.Orders[floor][button] = et.SimpleOrder{ID, bEvent}
 	return elev
 }
-
