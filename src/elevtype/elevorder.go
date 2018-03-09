9ad2ae74b9a1d5e7d4882ac85d0e0f243201f83b
Eirik Vesterkjær <eve.eirik@gmail.com>
2018-03-09 16:53:41 +0100

    Added SimpleOrder and Order interface

--- src/elevtype/elevorder.go
+++ src/elevtype/elevorder.go
@@ -11,6 +11,7 @@ const (
 	Finished OrderStatus = 2
 )
 
+// https://stackoverflow.com/questions/35660467/how-to-access-fields-of-a-json-in-go
 type ElevOrder struct {
 	// ID of request IP+timestamp+ButtonEvent
 	Id string
@@ -25,6 +26,70 @@ type ElevOrder struct {
 	// Assigned to elev @ IP
 	Assignee string
 }
+
+type SimpleOrder struct {
+	// ID of request IP+timestamp+ButtonEvent
+	Id string
+	// Floor & order type
+	Order ButtonEvent
+}
+
+type GeneralOrder interface {
+	ToSimpleOrder() SimpleOrder
+	//IsSimpleOrder() bool
+	IsEmpty() bool
+	IsActive() bool
+	IsSame(other GeneralOrder) bool
+	GetID() string
+	GetFloor() int
+	GetButton() ButtonType
+	GetOrder() ButtonEvent
+}
+
+func (o SimpleOrder) ToSimpleOrder() SimpleOrder {
+	return o
+}
+func (o ElevOrder) ToSimpleOrder() SimpleOrder {
+	return SimpleOrder{Id: o.Id, Order: o.Order}
+}
+
+//func (o SimpleOrder) IsSimpleOrder() bool {
+//	return true
+//}
+//func (o ElevOrder) IsSimpleOrder() bool {
+//	return false
+//}
+func (o SimpleOrder) IsEmpty() bool {
+	return o.Id == ""
+}
+func (o ElevOrder) IsEmpty() bool {
+	return o.Id == ""
+}
+
+func (o SimpleOrder) IsActive() bool {
+	return o.Id != ""
+}
+func (o ElevOrder) IsActive() bool {
+	return o.Id != "" && o.Status == Accepted
+}
+func (o SimpleOrder) IsSame(other GeneralOrder) bool {
+	return o.GetID() == other.GetID()
+}
+func (o ElevOrder) IsSame(other GeneralOrder) bool {
+	return o.GetID() == other.GetID()
+}
+func (o SimpleOrder) GetID() string {
+	return o.Id
+}
+func (o ElevOrder) GetID() string {
+	return o.Id
+}
+func (o SimpleOrder) GetFloor() int         { return o.Order.Floor }
+func (o ElevOrder) GetFloor() int           { return o.Order.Floor }
+func (o SimpleOrder) GetButton() ButtonType { return o.Order.Button }
+func (o ElevOrder) GetButton() ButtonType   { return o.Order.Button }
+func (o SimpleOrder) GetOrder() ButtonEvent { return o.Order }
+func (o ElevOrder) GetOrder() ButtonEvent   { return o.Order }
 
 func EmptyOrder() ElevOrder {
 	return ElevOrder{
