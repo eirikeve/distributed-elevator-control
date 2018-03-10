80578e98521099c875fda09b9a9eb7e54f20d9a6
Ole Petter <olepno@stud.ntnu.no>
2018-03-10 15:58:56 +0100

    Started to clean heartbeat. Still some bugs when running test

--- src/elevnetwork/elevheartbeat.go
+++ src/elevnetwork/elevheartbeat.go
@@ -1,12 +1,9 @@
 package elevnetwork
 
 import (
-	"encoding/json"
 	"fmt"
 	"time"
 
-	"./bcast"
-	"./localip"
 	p "./peers"
 	et "../elevtype"
 )
@@ -15,121 +12,28 @@ import (
 const HEARTBEATINTERVAL = 30 * time.Millisecond
 const HEARTBEATTIMEOUT = 10 * HEARTBEATINTERVAL
 
-type HeartbeatMessage struct {
-	//Status string `json:"status"`
-	// If status is to be used, it must be implemented
-	LocalIP string `json:"LocalIP"`
-}
+// Variable
+var signalHeartBeatToStop chan bool
 
-type Peer struct {
-	ID          HeartbeatMessage `json:"ID"`
-	ReceiveTime time.Time        `json:"ReceiveTime"`
-}
 
-// ***GLOBAL VARIABLES***
+func startHeartBeat(){
+	signalHeartBeatToStop = make(chan bool)
+	port := 20102
+	go runHeartBeat(port,"Hellu", signalHeartBeatToStop)
 
-// List of Peer members of the Network, by IP
-var listPeers []Peer
-
-// List containg information about last Heartbeat Message received by Peer members
-// ***FUNCTIONS***
-
-// Transmits a heartbeat containing a HeartbeatMessage for each HEARTBEATINTERVAL
-//@arg port: Transmit on given port
-func udpSendHeartBeat(port int, transMsgCh chan HeartbeatMessage) {
-	locIP, _ := localip.LocalIP()
-	if locIP == "" {
-		println("Could not find Local IP address.")
-		println("No error handling impleneted")
-	} else {
-		//Wait time for next transmission
-		time.Sleep(HEARTBEATINTERVAL)
-		//Wrtie to transmission channel
-		transMsgCh <- HeartbeatMessage{locIP}
-
-		//Transmit content on msg to port
-		bcast.Transmitter(port, transMsgCh)
-	}
-}
-
-/* Receive HeartbeatMessage from broadcasters
-* @arg port: Listen on given port
-* @arg recvMsgCh: Receive on given channel
- */
-func udpRecvHeartBeat(port int, recvMsgCh chan HeartbeatMessage) {
-	//Listening on network and receives message
-	bcast.Receiver(port, recvMsgCh)
-
-	/* Not needed? [Todo]
-	recvMsg := <- recvMsgCh
-	if recvMsg.LocalIP == ""{
-		println("Gotcha")
-	}else{
-		println("Missing")
-	}*/
 
 }
 
+func stopHeartBeat(){
+	signalHeartBeatToStop <- true
+}
 /*
-* Runs the heartbeat protocol, which monitors the participants of
-* the network.
-* @arg port: Broadcast on given port
-* @arg exsistingPeersCh: List of Peers in newtork Channel			[Todo: Missing - but will most likely be implemented]
- */
-func runHeartBeat(port int, existingPeersCh chan []Peer) {
-
-	startTime := time.Now()
-	lastTranmissionTime := time.Now()
-
-	sendMsgCh := make(chan HeartbeatMessage, 2)
-	recvMsgCh := make(chan HeartbeatMessage, 2)
-
-	go udpRecvHeartBeat(port, recvMsgCh)
-	for time.Now().Sub(startTime) < time.Second*10 {
-
-		if time.Now().Sub(lastTranmissionTime) > HEARTBEATINTERVAL {
-			go udpSendHeartBeat(port, sendMsgCh)
-			lastTranmissionTime = time.Now()
-
-		}
-
-		select {
-		case msg := <-recvMsgCh:
-			println("Messagtime.Now().Sub(startTime) < time.Second*10e received")
-			jsonMsg, _ := json.Marshal(msg)
-			println("Heartbeat Message:" + string(jsonMsg))
-			addToPeerList(msg, &listPeers, listPeers)
-			existingPeersCh <- listPeers
-		default:
-			updateToPeersList(&listPeers, listPeers)
-			existingPeersCh <- listPeers
-
-		}
-
-	}
-
-	//Print to see if listPeers is correct
-	println("Loop completed!")
-	for _, element := range listPeers {
-		peerJsonMsg, _ := json.Marshal(element)
-		println(string(peerJsonMsg))
-	}
-
-}
-
-//}
-/*
- * THIS IS THE VERSION WE'RE USING
-
- * Runs the heartbeat protocol wich nitors the participant of the netowrk.
+ * Runs the heartbeat protocol wich monitors the participant of the netowrk.
  * Version 2: Uses the given peers functions
- * @arg port: roadcast on given port
+ * @arg port: broadcast on given port
  * @arg PeersCh: List of current peers in network on channel
- *
  */
-func runHeartBeatVol2(port int, heartbeatMsg string) {
-
-	startTime := time.Now()
+func runHeartBeat(port int, heartbeatMsg string, signalHeartBeat <- chan bool) {
 	lastTranmissionTime := time.Now()
 
 	recvPeerCh := make(chan et.PeerUpdate)
@@ -139,63 +43,28 @@ func runHeartBeatVol2(port int, heartbeatMsg string) {
 	go p.Transmitter(port, heartbeatMsg, sendPeerCh)
 	go p.Receiver(port, recvPeerCh)
 
-	for time.Now().Sub(startTime) < time.Second*10 {
+	for {
+		
 
 		if time.Now().Sub(lastTranmissionTime) > HEARTBEATINTERVAL {
 			sendPeerCh <- true
 			lastTranmissionTime = time.Now()
 		}
+
 		select {
 
 		case msg := <-recvPeerCh:
 			fmt.Printf("ID: %v \n", msg.Peers)
+		
+		case <-signalHeartBeatToStop:
+			println("exit")
+			return
 
 		default:
 			sendPeerCh <- false
 		}
 
 	}
+	
 
-}
-
-/*
-* Updates the P2P network,
-* Add new received IP-address
-* Update timestap from received messages
-* @arg msg: Contains the Heartbeat Message that was received
-* @arg, @arg pointerList, List: Contains the list of already exsisting members of the network
-* [Todo]Ops: The same list is taken as two different parameters to acheive call-by-reference for indexing and appending
- */
-func addToPeerList(msg HeartbeatMessage, pointerList *[]Peer, list []Peer) {
-
-	//Add new elements and update timestap from received messages
-	addElement := true
-	currentPeer := Peer{msg, time.Now()}
-	if *pointerList == nil {
-
-		*pointerList = append(*pointerList, currentPeer)
-	} else {
-		for index, element := range *pointerList {
-			if element.ID == msg {
-				addElement = false
-				list[index].ReceiveTime = time.Now()
-				break
-
-			}
-		}
-		if addElement {
-			*pointerList = append(*pointerList, currentPeer)
-		}
-	}
-
-}
-
-//Remove P2P members, that we have not got any response from within HARTBEATTIMEOUT
-// OPS what happen with index and element if one P2P connection is removed?!?!?
-func updateToPeersList(pointerList *[]Peer, list []Peer) {
-	for index, element := range list {
-		if time.Now().Sub(element.ReceiveTime) >= HEARTBEATTIMEOUT {
-			*pointerList = append(list[:index], list[index+1:]...)
-		}
-	}
 }
--- src/elevnetwork/heartbeat_test.go
+++ src/elevnetwork/heartbeat_test.go
@@ -3,9 +3,7 @@ package elevnetwork
 import(
 	"testing"
 	"./localip"
-	"./bcast"
 	"time"
-	"encoding/json"
 	
 	
 	
@@ -22,69 +20,27 @@ func TestLocalIP(t *testing.T){
 
 }
 
-
-
-// Testing UDP Transmission and Reception
-
-func TestUdpbcast(t *testing.T){
-
-	port := 20008
-	localip,_:= localip.LocalIP()
-	sendMsgCh := make(chan HeartbeatMessage)
-	recvMsgCh := make(chan HeartbeatMessage)
-
-	go bcast.Transmitter(port,sendMsgCh)
-	go bcast.Receiver(port,recvMsgCh)
-
-	i:= 0
-	start := time.Now()
-	for {
-
-		sendMsgCh <- HeartbeatMessage{localip}
-		time.Sleep(time.Second*1)
-		msg := <- recvMsgCh	
-		jsonMsg,_ := json.Marshal(msg)
-		println(string(jsonMsg))
-		i++
-		if time.Second*1 < time.Now().Sub(start){
-			return		
-		}
-	}
-}
 /*
-* Test for how the heartbeat protocol should approximatley be run in either
-* the Network handler or Network State module.
-* Makes a local list of Peers, which is continously updated from the Peers channel.
-*/
-func TestUdpHeartbeat(t *testing.T){
-	port := 20008
-	PeersCh := make(chan []Peer ) 
-	go runHeartBeat(port,PeersCh)
-	for{
-		select{
-		case  peerNetworkList:= <-PeersCh:
-			for _,element :=range peerNetworkList{
-				peerJsonMsg,_ := json.Marshal(element)
-				println(string(peerJsonMsg))
-			}
+ * A Heartbeat protocol test
+ * runs two runHeartBeat functions simulatainiously, with different IP
+ * Both ID's should appear in list
+ */
+func TestHeartBeat(t *testing.T){
+
+	startTime := time.Now()
+
+	go startHeartBeat()
+	
+	for time.Now().Sub(startTime) < time.Second*10{
 		
-		default:
+		if time.Now().Sub(startTime) > time.Second*5{
+			println("Completed99")
+			stopHeartBeat()
+			
 		}
+
 	}
 	
-}
-/*
- * Another test for the hearbeat protocol, but uses runHeartBeatVol2 instead of
- * runHeartHeat, which are implented somewhat differently.
- */
-func TestHeartBeatVol2(t *testing.T){
-	port := 20102
-	ID1,_ := localip.LocalIP()
-	ID2 := "255.255.255.255"	//Random IP address
-	go runHeartBeatVol2(port,ID1)
-	time.Sleep(time.Second*2)
-	go runHeartBeatVol2(port,ID2)
-	time.Sleep(time.Second*12)
 	
 }
 
