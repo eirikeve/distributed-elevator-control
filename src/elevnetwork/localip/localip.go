f1fb867d1f12982233d9df167740e8c305ab5e1e
emilhThyri <emilThyri@gmail.com>
2018-04-10 18:05:52 +0200

    Created LocalID and LocalIPWithPort

--- src/elevnetwork/localip/localip.go
+++ src/elevnetwork/localip/localip.go
@@ -1,11 +1,14 @@
 package localip
 
 import (
+	et "../../elevtype"
 	"net"
+	"strconv"
 	"strings"
-	ed "../../elevdriver" //@SIM
 )
+
 var localIP string
+var localID int32
 
 func LocalIP() (string, error) {
 	if localIP == "" {
@@ -14,7 +17,36 @@ func LocalIP() (string, error) {
 			return "", err
 		}
 		defer conn.Close()
-		localIP = strings.Split(conn.LocalAddr().String(), ":")[0]+":"+ed.PortNum // @SIM added '":"+ed.PortNUM"'
+		localIP = strings.Split(conn.LocalAddr().String(), ":")[0]
 	}
 	return localIP, nil
+}
+
+func LocalID() (int32, error) {
+	_, err := LocalIP()
+	if localID == 0 {
+		if localIP == "" {
+			return 0, err
+		} else {
+
+			splitIP := strings.Split(localIP, ".")
+			v, _ := strconv.Atoi(splitIP[len(splitIP)-1])
+			localID = int32(v)
+			v2, _ := strconv.Atoi(et.SystemIpPort)
+			localID += int32(v2)
+
+			if localID == 0 {
+				localID = 256
+			}
+		}
+	}
+	return localID, nil
+}
+
+func LocalIPWithPort() (string, error) {
+	_, err := LocalIP()
+	if localIP == "" {
+		return "", err
+	}
+	return localIP + ":" + et.SystemIpPort, nil
 }
