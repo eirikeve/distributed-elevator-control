ba5a12061b3419d786eb9741788b3b32d5b7ead6
Eirik Vesterkjær <eve.eirik@gmail.com>
2018-04-15 12:43:42 +0200

    Removed some unused files & moved log init to setup.go

--- src/setup.go
+++ src/setup.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strconv"
+	"time"
 
 	el "./elevlog"
 	et "./elevtype"
@@ -36,4 +39,30 @@ func parseCmdLineArgs() {
 func setupLog() {
 	el.InitLog(logLevel, logToFileInsteadOfBash)
 	log.Debug("main setupLog: log initialized")
+}
+
+func initLog(level log.Level, toFile bool) {
+	if toFile {
+		// Set name of current log output to the current UNIX timestamp
+		filename := strconv.FormatInt(time.Now().Unix(), 10) + ".elevlog"
+		logFile, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0755)
+		if err != nil {
+			// Could not open file
+			log.WithFields(log.Fields{
+				"Error": err.Error(),
+			}).Error("initLog: Could not open log output file. Defaulting to bash output.")
+		} else {
+			log.SetOutput(logFile)
+		}
+	}
+	if log.PanicLevel <= level && level <= log.DebugLevel {
+		log.SetLevel(level)
+	} else {
+		// Invalid level input
+		log.SetLevel(log.InfoLevel)
+		log.WithFields(log.Fields{
+			"Log level": level,
+		}).Warning("initLog: Invalid log level input. Defaulting to log.InfoLevel")
+	}
+
 }
