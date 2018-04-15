d058c16488032bd5a9c394e9524987191ec16927
Kim Isak Olsen <kiisol@users.noreply.github.com>
2018-04-15 13:15:35 +0200

    Removed NetState(unused struct).

--- src/elevtype/sysstatestructs.go
+++ src/elevtype/sysstatestructs.go
@@ -1,7 +1,5 @@
 package elevtype
 
-import "time"
-
 type ElevState struct {
 	// ID is the local IP of the computer corresponding to an ElevState
 	ID int32 `json: "ID"`
@@ -11,11 +9,4 @@ type ElevState struct {
 	// ExtendedOrders is the Networkhandler's orders in the system corresponding to an ElevState
 	CurrentOrders  [NumFloors][NumButtons]ElevOrder `json: "CurrentOrders"`
 	FinishedOrders [12]ElevOrder                    `json: "FinishedOrders"`
-}
-
-type NetState struct {
-	// LastUpdate is the last time this NetState was updated
-	LastUpdate time.Time
-	// P is the active Peers and Lost peers of this computer
-	P PeerUpdate
 }
